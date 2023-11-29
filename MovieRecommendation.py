fdc1afb985dff0ae204115f715f238ab8188d8e6
ryanfriedrich1 <ryan_friedrich@hotmail.com>
2023-11-29 16:28:39 -0500

    added tree traversal and user input

--- MovieRecommendation.py
+++ MovieRecommendation.py
@@ -1,4 +1,42 @@
-movieGenres = ["Action", "Adventure", "Comedy", "Horror", "Romance", "Sci-fi", "History", "Holiday", "Documentary"]
+# TreeNode Class
+class TreeNode:
+    def __init__(self, genre):
+        self.genre = genre
+        self.movies = []
+
+    def add_child(self, node):
+        self.movies.append(node)
+
+    def recommendation(self):
+        loop = True
+        while loop:   
+            print("What genre of movie would you like to watch?")
+            userInput = input("Type the beginning of that genre and press enter so see movie recommendations\n")
+            genres = autoComplete(userInput.lower())
+            while len(genres) > 1:
+                print("With those beginning letters your choices are: ")
+                for x in genres:
+                    print(x.genre)
+                userInput = input("What genre of movie do you want to watch?\n")
+                genres = autoComplete(userInput.lower())
+            userInput = input("The only option with those beginning letters is {0}. Do you want to look at {0} movies? Enter (y/n): ".format(genres[0].genre))
+            if userInput.lower() == "y":
+                print("Genre | Title | Release date | rotten tomato score | movie length")
+                for x in genres[0].movies:
+                    print(x)
+            userInput = input("Would you like to search for more movie recommendations? Enter (y/n): ")
+            if userInput.lower() == "n":
+                loop = False
+        print("Thank you for using Movie Recommendations V1.0!")
+
+def autoComplete(input):
+    genreAutoComplete = []
+    for x in movieGenres:
+        if x.genre.startswith(input):
+            genreAutoComplete.append(x)
+    return genreAutoComplete
+
+movieGenres = [TreeNode("action"), TreeNode("adventure"), TreeNode("comedy"), TreeNode("horror"), TreeNode("romance"), TreeNode("sci-fi"), TreeNode("history"), TreeNode("holiday"), TreeNode("documentary")]
 
 # genre, movie title, release date, audience score, movie length
 movieData = [["Action", "The Killer", "November 10 2023", "59%", "1h 58m"],
@@ -46,4 +84,13 @@ movieData = [["Action", "The Killer", "November 10 2023", "59%", "1h 58m"],
              ["Documentary", "Bye Bye Barry", "November 21 2023", "98%", "1h 32m"],
              ["Documentary", "Omoiyari: A Song Fily By Skishi Bashi", "November 21 2023", "93%", "1h 33m"],
              ["Documentary", "American Symphony", "November 29 2023", "80%", "1h 40m"],]
+
+# setting up tree structure
+# each genre is a tree branch with the 5 movies in the genre as its children
+for x in range(len(movieGenres)):
+   counter = x * 5
+   for y in range(5):
+       movieGenres[x].add_child(movieData[counter + y])
+
+movieGenres[0].recommendation()
 
